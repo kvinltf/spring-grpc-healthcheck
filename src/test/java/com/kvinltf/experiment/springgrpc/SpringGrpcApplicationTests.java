883a218802a5f4193bdff97924613416404dec8c
Kevin Lu <kevin999990@gmail.com>
2025-03-10 21:16:06 +0800

    init

---
+++ src/test/java/com/kvinltf/experiment/springgrpc/SpringGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kvinltf.experiment.springgrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
